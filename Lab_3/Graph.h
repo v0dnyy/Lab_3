f95f46ce118b9bb01f1e08f928a3be5701f70d4b
Степан Водянов <2020-01161@students.ssau.ru>
2022-05-30 22:07:42 +0400

    Add a function that returns a vertex index

--- Lab_3/Graph.h
+++ Lab_3/Graph.h
@@ -5,15 +5,24 @@ template<typename TVertex, typename TEdge >
 class Graph {
 private:
 	struct destination {
-		int id;
+		int id_d;
 		TVertex dest;
 		TEdge edge;
 	};
 	struct vertex
 	{
-		int id;
-		TVertex source;
+		int id_v;
+		TVertex data;
 		vector<destination> dest;
 	};
-	vector<vertex> table; 
+	vector<vertex> table;
+public:
+	int Index(TVertex Temp_Vertex)
+	{
+		for (size_t i = 0; i < table.size(); i++)
+		{
+			if (table[i].data == Temp_Vertex) return table[i].id_v;
+		}
+		return -1;
+	}
 };
