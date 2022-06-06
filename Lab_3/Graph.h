a305bfc4971c8d1cfed669618bf0e2eedebe72de
Степан Водянов <2020-01161@students.ssau.ru>
2022-06-07 01:55:01 +0400

    rework graph class and its methods

--- Lab_3/Graph.h
+++ Lab_3/Graph.h
@@ -47,3 +47,106 @@ struct Vertex{
 	}
 	Vertex(const string& str, const size_t& am) : id(str), amount(am), colour(0) {}
 };
+template <typename TVertex, typename TEdge, typename equal = equal_to<Vertex>>
+class Graph {
+	vector<vector<TEdge>> edge;
+	vector<TVertex> vertex;
+	size_t count;
+	bool check(vector<TVertex>& tmp, const string& str) const {
+		for (size_t i = 0; i < tmp.size(); ++i) {
+			if (str == tmp[i].id) return true;
+		}
+		return false;
+	}
+	int checker(const string& str) const {
+		for (size_t i = 0; i < vertex.size(); ++i) {
+			if (str == vertex[i].id) return i;
+		}
+		return -1;
+	}
+public:
+	int Get_ID(const TVertex& rhs) const {
+		equal compare;
+		for (size_t i = 0; i < vertex.size(); ++i)
+		{
+			if (compare(rhs, vertex[i]))
+				return i;
+		}
+		return -1;
+	}
+	int Get_ID(const string& rhs) const {
+		for (size_t i = 0; i < vertex.size(); ++i)
+		{
+			if (vertex[i].id == rhs)
+				return i;
+		}
+		return -1;
+	}
+	Graph() {
+		count = 0;
+	}
+	bool findVertex(const TVertex& f) const {
+		equal compare;
+		for (auto it : vertex) {
+			if (compare(it, f)) return true;
+		}
+		return false;
+	}
+	void addVertex(const TVertex& newVertex)
+	{
+		if (findVertex(newVertex) == true) return;
+		vertex.push_back(newVertex);
+		vector<TEdge> tmp(0);
+		edge.push_back(tmp);
+		count++;
+	}
+	void deleteVertex(const TVertex& target) {
+		equal compare;
+		int ind = -1;
+		string temp;
+		for (size_t i = 0; i < vertex.size(); ++i) {
+			if (compare(vertex[i], target)) {
+				ind = i;
+				temp = vertex[i].id;
+				vertex.erase(vertex.begin() + i);
+				break;
+			}
+		}
+		if (ind == -1) return;
+		edge[ind].erase(edge[ind].begin(), edge[ind].end());
+		for (size_t i = 0; i < edge.size(); ++i) {
+			for (size_t j = 0; j < edge[i].size(); ++j) {
+				if (edge[i][j].dest == temp) {
+					edge[i].erase(edge[i].begin() + j);
+				}
+			}
+		}
+		--count;
+	}
+	void addEdge(const TVertex& src, const TVertex& dst, const TEdge& newEdge) {
+		if (findVertex(src) == false || findVertex(dst) == false) return;
+		equal compare;
+		for (size_t i = 0; i < vertex.size(); ++i) {
+			if (compare(src, vertex[i])) {
+				edge[i].push_back(newEdge);
+				return;
+			}
+		}
+	}
+	void deleteEdge(const TVertex& src, const TVertex& dst) {
+		if (findVertex(src) == false || findVertex(dst) == false) return;
+		int ind = -1;
+		equal compare;
+		for (size_t i = 0; i < vertex.size(); ++i) {
+			if (compare(vertex[i], src)) {
+				ind = i;
+				break;
+			}
+		}
+		for (size_t i = 0; i < edge[ind].size(); ++i) {
+			if (edge[ind][i].dest == dst.id) {
+				edge[ind].erase(edge[ind].begin() + i);
+			}
+		}
+	}
+};
